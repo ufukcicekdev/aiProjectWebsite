7cc5e93d665312a31305ca1f623511958a01ba1e
EAGLEEARTH <ufukcicek199@gmail.com>
2023-08-20 14:27:26 +0300

    title changes

--- aiwebsite/models.py
+++ aiwebsite/models.py
@@ -59,7 +59,7 @@ class Tag(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    Description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     slug = AutoSlugField(populate_from = 'title', unique = True)
     pricingModel = models.ForeignKey(Pricing,on_delete=models.SET_NULL,null=True)
     tooltags = models.ManyToManyField(Tag)
