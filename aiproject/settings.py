fb3b0d31a57d20e284bafb964a802dccc613d9eb
EAGLEEARTH <ufukcicek199@gmail.com>
2023-08-22 22:45:53 +0300

    debug false added

--- aiproject/settings.py
+++ aiproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h-3hp@8syl=k2twx6wdzp$s1z0z6e2y*jllq+mx9_0e450f+4-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
