5bbab19b6a4128ac6616cabf01e97d6129932ee6
EAGLEEARTH <ufukcicek199@gmail.com>
2023-08-23 00:56:46 +0300

    sitemap added

--- aiwebsite/sitemap.py
+++ aiwebsite/sitemap.py
@@ -30,3 +30,18 @@ class PricingLinkSiteMap(Sitemap):
 
     def location(self,obj):
         return f"/pricing/{obj.slug}"
+    
+
+
+# class ProductLinkSiteMap(Sitemap):
+#     changefreq = "weekly"
+#     priority = 0.8
+#     protocol = 'https'
+
+#     def items(self):
+#         return Product.objects.filter(is_active=True)
+
+#     def location(self, obj):
+#         category_slug = obj.category.slug  # Burada ilişkili kategori slug'ını alıyoruz
+#         product_slug = obj.slug
+#         return f"/category/{category_slug}/product/{product_slug}/"
